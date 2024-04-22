00f61878c17e374557d1526f9317032023a0d6e1
jadewang425 <jadewang425@gmail.com>
2024-04-22 00:16:16 -0400

    misc

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Grid from './components/grid';
 
 function App() {
+  const numRows = 30
+  const numCols = 30
+
+  const [generation, setGeneration] = useState(0)
+  const [grid, setGrid] = useState(
+      Array(numRows).fill().map(() => Array(numCols).fill(false))
+  )
+
+  const selectCell = (row, col) => {
+    let currGrid = arrClone(grid)
+    currGrid[row][col] = !currGrid[row][col]
+    setGrid(currGrid)
+  }
+
+  const random = () => {
+    let initGrid = arrClone(grid)
+    for (let i = 0; i < numRows; i++) {
+      for (let j = 0; j < numCols; j++) {
+        initGrid[i][j] = Math.floor(Math.random() * 2)
+      }
+    }
+    setGrid(initGrid)
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Conway's Game of Life</h1>
+      <div className='btns'>
+        <button>Play</button>
+        <button onClick={random}>Random</button>
+      </div>
+      <Grid 
+        grid={grid}
+        rows={numRows}
+        cols={numCols}
+        selectCell={selectCell}
+      />
     </div>
   );
+}
+
+function arrClone(arr) {
+  return JSON.parse(JSON.stringify(arr))
 }
 
 export default App;
