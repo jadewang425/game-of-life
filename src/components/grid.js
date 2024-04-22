20cf5ce90193ac5fb3e0a9427ff5cee15abfd556
jadewang425 <jadewang425@gmail.com>
2024-04-21 23:37:47 -0400

    added cells & some CSS

--- src/components/grid.js
+++ src/components/grid.js
@@ -1,7 +1,37 @@
-export default function Grid () {
+
+const Cell = (props) => {
+    const { cellClass, key } = props
     return (
-        <div>
-            This is the grid
+        <div className={cellClass} id={key}></div>
+    )
+}
+
+export default function Grid (props) {
+    const { grid, rows, cols } = props
+    const width = cols * 16
+    let rowsArr = []
+
+    let cellClass = ""
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            let cellId = i + "_" + j
+            cellClass = grid[i][j] ? "cell alive" : "cell dead"
+
+            rowsArr.push(
+                <Cell 
+                    cellClass={cellClass}
+                    key={cellId}
+                    cellId={cellId}
+                    row={i}
+                    col={j}
+                />
+            )
+        }
+    }
+
+    return (
+        <div className="board" style={{width: width}}>
+            {rowsArr}
         </div>
     )
 }
